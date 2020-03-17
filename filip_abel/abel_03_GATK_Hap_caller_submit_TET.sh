fb23431ff777c7a3d8cee0b40c60d458238d507d
V-Z <vojta@trapa.cz>
2020-03-17 12:41:43 +0100

    Removal of old scripts, scripts moved to bin, added license and readme, small updates.

--- filip_abel/abel_03_GATK_Hap_caller_submit_TET.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --account=uio
-#SBATCH --time=50:0:0
-#SBATCH --mem-per-cpu=200M
-source /cluster/bin/jobsetup
-module purge   # clear any inherited modules
-set -o errexit # exit on errors
-
-arrayrun 1-188 abel_03_GATK_Hap_caller_worker_TET.sh   #change N of arrays based on N of samples=dirs to process
